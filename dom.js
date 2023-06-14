ae87c5fc9115508987feebd6c4e19eec4ca1696c
OrigamiStarz <40199724+OrigamiStarz@users.noreply.github.com>
2023-06-14 12:24:27 -0700

    up to CSS animation

--- dom.js
+++ dom.js
@@ -7,9 +7,16 @@ let tables = document.getElementsByClassName('schedule_table');
 selection[0].addEventListener('click', function(){
     tables[0].className = "schedule_table";
     tables[1].className = "schedule_table hide";
+    tables[2].className = "schedule_table hide";
 });
 
 selection[1].addEventListener('click', function(){
     tables[1].className = "schedule_table";
     tables[0].className = "schedule_table hide";
+    tables[2].className = "schedule_table hide";
+});
+selection[2].addEventListener('click', function(){
+    tables[2].className = "schedule_table";
+    tables[0].className = "schedule_table hide";
+    tables[1].className = "schedule_table hide";
 });
