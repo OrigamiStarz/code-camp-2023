7ebe35e231f7d350a188801c19c7cf9e818a0269
OrigamiStarz <40199724+OrigamiStarz@users.noreply.github.com>
2023-06-14 11:11:28 -0700

    first commit

---
+++ dom.js
@@ -0,0 +1,15 @@
+//Use this for DOM ONLY!!!!!!!!!!!!
+
+//----------schedule DOM-------------
+let selection = document.getElementsByClassName('week_button');
+let tables = document.getElementsByClassName('schedule_table');
+
+selection[0].addEventListener('click', function(){
+    tables[0].className = "schedule_table";
+    tables[1].className = "schedule_table hide";
+});
+
+selection[1].addEventListener('click', function(){
+    tables[1].className = "schedule_table";
+    tables[0].className = "schedule_table hide";
+});
---
+++ greet.js
@@ -0,0 +1,6 @@
+setTimeout(
+    function(){
+        document.getElementById('GreetContainer').style.display = 'none';
+        document.getElementById('ActualContent').style.display = 'block';
+    }, 2900);
+
